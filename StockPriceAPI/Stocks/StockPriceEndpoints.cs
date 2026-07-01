9f4c78b1db2b9b1a4325565f7ae6ba50e9d533f6
Morten Tønnesen <toennesen123@gmail.com>
2026-07-01 13:42:46 +0200

    feat(docker): add docker stuff
    hey

---
+++ StockPriceAPI/Stocks/StockPriceEndpoints.cs
@@ -0,0 +1,28 @@
+﻿namespace StockPriceAPI.Stocks
+{
+    public static class StockPriceEndpoints
+    {
+        public static void MapStockPriceEndpoints(this IEndpointRouteBuilder routes)
+        {
+            var group = routes.MapGroup("api/stocks");
+
+            group.MapGet("/{ticker}", GetLatestStockPrice)
+                .WithName(nameof(GetLatestStockPrice))
+                .WithOpenApi();
+        }
+
+        private static async Task<IResult> GetLatestStockPrice(string ticker, StockService stockService, CancellationToken cancellationToken)
+        {
+            try
+            {
+                var result = await stockService.GetLatestStockPrice(ticker, cancellationToken);
+
+                return Results.Ok(result);
+            }
+            catch (OperationCanceledException)
+            {
+                return Results.StatusCode(499);
+            }
+        }
+    }
+}
